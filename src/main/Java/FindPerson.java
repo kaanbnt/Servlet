291ab3940708a58aa0b719200ea5dafe18ff49ec
KAAN BİNAT <58415416+kaanbnt@users.noreply.github.com>
2019-12-13 08:50:29 +0300

    Initial commit

---
+++ src/main/Java/FindPerson.java
@@ -0,0 +1,54 @@
+import javax.servlet.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindPerson implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    static List<Person> personelList = new ArrayList<>();
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        String islem = servletRequest.getParameter("islem");
+        System.out.println(islem);
+        if (islem.equals("kaydet")) {
+            String ad = servletRequest.getParameter("ad");
+            String soyad = servletRequest.getParameter("soyad");
+            Person person = new Person();
+            person.setAd(ad);
+            person.setSoyad(soyad);
+            personelList.add(person);
+        } else if (islem.equals("listele")) {
+            //Person person=new Person();
+            for (Person person : personelList) {
+                String html = person.getAd()+" "+person.getSoyad()+"\n";
+                servletResponse.setCharacterEncoding("UTF-8");
+                servletResponse.getWriter().write(html);
+                System.out.println(person);
+            }
+
+        } else {
+            System.out.println("Lütfen Yapmak İstediğiniz İşlemi Seçiniz! ");
+        }
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/Java/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String ad;
+    private String soyad;
+//
+//    public Person()
+//    {
+//
+//    }
+//    public Person(String ad, String soyad) {
+//        this.ad = ad;
+//        this.soyad = soyad;
+//    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+}
